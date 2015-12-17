4372dfc242a725f4fb332910a25d71744b8844ca
DartChief <dartchief@yandex.ru>
2015-12-18 02:05:41 +0300

    Second Commit

--- Homework_1/putter - funcStyle.rb
+++
@@ -1,6 +0,0 @@
- sorted_lines = Array[].push(File.readlines('people.csv').sort { |a, b| a <=> b })
-  File.open('people.csv', 'w') do |files|
-    sorted_lines.each do |ss|
-      files.puts ss
-    end
-  end
