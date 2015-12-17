170cdfcbb3ddd64d7763e642b33ac2e76bf0ecca
DartChief <dartchief@yandex.ru>
2015-12-18 02:43:49 +0300

    Prototype

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
   def toggle_like_button(post)
     user = User.first
     if user.flagged?(post, :like)
-      link_to 'Unlike', like_post_path(post), class: 'btn btn-default'
+      link_to 'Dislike', like_post_path(post), class: 'btn btn-default'
     else
       link_to 'Like', like_post_path(post), class: 'btn btn-default'
     end
